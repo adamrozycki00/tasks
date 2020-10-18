1331e48ec9c60bff21d4853b5e6f996d191c3e4a
Adam Rozycki <adamrozycki00@gmail.com>
2020-10-18 21:09:44 +0200

    update methods TaskController.getTasks and TaskController.getTask

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -1,5 +1,6 @@
 package com.crud.tasks;
 
+import com.crud.tasks.domain.TaskDto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,8 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TasksApplication {
 
 	public static void main(String[] args) {
+
+		TaskDto taskDto = new TaskDto(Long.valueOf(1), "test title", "test content");
+		System.out.println(taskDto);
+
 		SpringApplication.run(TasksApplication.class, args);
 	}
 
 }
-
