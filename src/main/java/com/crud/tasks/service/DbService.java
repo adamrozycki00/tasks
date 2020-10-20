c3332d50f85e63b6a15b0e27df01a80cb15a1a0e
Adam Rozycki <adamrozycki00@gmail.com>
2020-10-20 21:25:09 +0200

    refactor method TaskController.getTask

--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class DbService {
@@ -17,8 +18,8 @@ public class DbService {
         return repository.findAll();
     }
 
-    public Task getTaskById(Long id) {
-        return repository.findById(id).orElse(null);
+    public Optional<Task> getTask(Long id) {
+        return repository.findById(id);
     }
 
     public Task saveTask(Task task) {
