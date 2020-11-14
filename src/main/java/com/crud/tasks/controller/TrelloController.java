cc642a82bc43dc29f4ba596683314667d62650a1
Adam Rozycki <adamrozycki00@gmail.com>
2020-11-14 12:27:23 +0100

    Add class TrelloCardDTO

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -1,12 +1,9 @@
 package com.crud.tasks.controller;
 
-import com.crud.tasks.domain.TrelloBoardDTO;
 import com.crud.tasks.trello.client.TrelloClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
