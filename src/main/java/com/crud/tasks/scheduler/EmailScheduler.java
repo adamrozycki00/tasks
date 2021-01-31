1635bd93e339d6c721571a52f8726f3cad7f60e0
Adam Rozycki <adamrozycki00@gmail.com>
2021-01-31 18:09:33 +0100

    Add mailing scheduler

---
+++ src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
@@ -0,0 +1,35 @@
+package com.crud.tasks.scheduler;
+
+import com.crud.tasks.config.AdminConfig;
+import com.crud.tasks.domain.Mail;
+import com.crud.tasks.repository.TaskRepository;
+import com.crud.tasks.service.SimpleEmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+
+public class EmailScheduler {
+
+    @Autowired
+    private SimpleEmailService emailService;
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @Autowired
+    private AdminConfig adminConfig;
+
+    private static final String SUBJECT = "Tasks: once a day email";
+
+//    @Scheduled(cron = "0 0 10 * * *")
+    @Scheduled(fixedDelay = 30_000)
+    public void sendInformationEmail() {
+        long size = taskRepository.count();
+        String messageProperEnding = (size == 1 ? " task." : " tasks.");
+        emailService.send(new Mail(
+                adminConfig.getAdminMail(),
+                SUBJECT,
+                "Currently in the database you have " + size + messageProperEnding
+        ));
+    }
+
+}
